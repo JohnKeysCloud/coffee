5e282304488bb3381911ceb235ea4d4290d9f9bd
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2023-10-28 04:12:29 -0400

    initial commit, prepare for launch 💭🚀

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/js/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true, // removes files that aren't in use
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(s[ac]ss)$/i,
+            use: [
+              // Creates 'style' nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+            // executes from bottom to top
+          },
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Cyclone Studios App',
+      minify: {
+        collapseWhitespace: false,
+        removeComments: false,
+      },
+      filename: 'index.html',
+      inject: 'head',
+      scriptLoading: 'defer',
+      hash: false, // add hash to the file so filename is different on every build
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'src'), // static files
+    port: 3000,
+    open: true, // open browser on server start
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Cyclone Studios App',
+      filename: 'index.html',
+      inject: 'head',
+      scriptLoading: 'defer',
+      hash: false, // add hash to the file so filename is different on every build
+      minify: {
+        collapseWhitespace: false,
+        removeComments: false,
+      },
+      template: './src/index.ejs',
+    }),
+  ],
+  devtool: 'inline-source-map',
+};
