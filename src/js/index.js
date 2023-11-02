32df8c9cc90e38c1cd29d752e25622df5f2966dd
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2023-11-02 18:23:26 -0400

    finish javascript landing page

--- src/js/index.js
+++ src/js/index.js
@@ -82,7 +82,7 @@ function createHeader() {
 
   const headerTitle = document.createElement('a');
   headerTitle.classList.add('header-title');
-  headerTitle.textContent = 'Coffee';
+  headerTitle.textContent = 'Cloud 8 Café';
 
   const headerRight = document.createElement('div');
   headerRight.classList.add('header-right');
@@ -113,18 +113,17 @@ function createDivider(classArray = []) {
   
   const pathData = 'M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z';
 
-  const svgNameSpace = 'http://www.w3.org/2000/svg';
   const shapeDivider = document.createElement('div');
   shapeDivider.classList.add(...classArray);
 
-  const shapeDividerSVG = document.createElementNS(svgNameSpace, 'svg');
+  const shapeDividerSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   // ? ns = namespace (svgNameSpace) is required for svg elements
   shapeDividerSVG.setAttributeNS(null, 'viewBox', '0 0 1200 120');
   shapeDividerSVG.setAttributeNS(null, 'preserveAspectRatio', 'none');
   shapeDividerSVG.setAttribute('aria-hidden', 'true');
   shapeDividerSVG.setAttribute('role', 'presentation');
   
-  const shapeDividerPath = document.createElementNS(svgNameSpace, 'path');
+  const shapeDividerPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   shapeDividerPath.classList.add('shape-fill');
   shapeDividerPath.setAttributeNS(null, 'd', pathData);
 
@@ -287,7 +286,7 @@ function renderHoursContent() {
 
     dayContainer.appendChild(operatingDay);
     dayContainer.appendChild(operatingHours);
-    
+
     hoursContentContainer.appendChild(dayContainer);
     hoursContentContainer.appendChild(horizontalRule);
   }
@@ -411,12 +410,80 @@ function createSectionThree() {
 
 function createFooter() {
   const footer = document.createElement('footer');
-  // const footerTextParts
+  const footerTextParts = [
+    '©',
+    ' ',
+    '{{footerYear}}',
+    ' ',
+    'Coffee by Cyclone Studios',
+  ];
 
+  const footerText = document.createElement('p');
+  footerTextParts.forEach((part) => {
+    if (part === '{{footerYear}}') {
+      const footerYear = document.createElement('span');
+      footerYear.classList.add('footer-year');
+      footerYear.textContent = new Date().getFullYear();
+
+      footerText.appendChild(footerYear);
+    } else {
+      footerText.appendChild(document.createTextNode(part));
+    }
+  });
   
+  const logoContainer = document.createElement('div');
+  logoContainer.classList.add('logo-container');
 
+  const contactData = {
+    phone: {
+      'href': 'tel:212-555-7813',
+      'aria-label': 'Call Us',
+      svgPathD:
+        'M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z',
+    },
+    instagram: {
+      'href': '#',
+      'aria-label': 'Instagram',
+      svgPathD:
+        'M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z',
+    },
+    email: {
+      href: 'mailto:coffee@cyclonestud.io',
+      'aria-label': 'Email Us',
+      svgPathD: 'M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z',
+    }
+  }
 
-  // const footerText = document.
+  for (const contactMethod in contactData) {
+    const methodData = contactData[contactMethod];
+
+    console.log(contactMethod);
+    console.log(contactData[contactMethod].href);
+    
+    const contactButtonLink = document.createElement('a');
+    contactButtonLink.classList.add('footer-link');
+    contactButtonLink.setAttribute('href', methodData.href);
+    contactButtonLink.setAttribute('aria-label', methodData['aria-label']);
+
+    const contactButtonSVG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    contactButtonSVG.classList.add('footer-logo');
+    contactButtonSVG.setAttributeNS(null, 'viewBox', '0 0 512 512');
+    contactButtonSVG.setAttributeNS(null, 'role', 'img');
+
+    const contactButtonSVGPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    contactButtonSVGPath.setAttributeNS(null, 'd', methodData.svgPathD);
+
+    contactButtonSVG.appendChild(contactButtonSVGPath);
+
+    contactButtonLink.appendChild(contactButtonSVG);
+
+    logoContainer.appendChild(contactButtonLink);
+  }
+
+  footer.appendChild(footerText);
+  footer.appendChild(logoContainer);
+
+  return footer;
 }
 
 function createMain() {
@@ -431,7 +498,7 @@ function createMain() {
 
 content.appendChild(createHeader(pageNames));
 content.appendChild(createMain());
-// content.appendChild(createFooter());
+content.appendChild(createFooter());
 
 // ! use when changing page contents
 // navLink.setAttribute('aria-current', 'page');
